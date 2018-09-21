e3c65bd70661d44130ac019aa273e1fa0d3619a5
mbacsamurai <mbacsamurai@hotmail.com>
2018-09-21 15:40:58 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'YeXxLICr94+ENA0R/DHH7QmdqRJPFqTzlDW+IjLdOjiH5cqI+M6wzWKkbnCVhLMrJXnPNQuWm+ueO0DMVXV7MWjvzEPVO20Q7tl3WXll9OOomizqXwg0e5Hqt+6Dm3ucAoeKKhCksu5jlJM2P2GwFQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
