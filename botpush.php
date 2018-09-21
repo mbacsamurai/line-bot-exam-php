ceae14359107bb5a58b74f9b3700b9e27688a925
mbacsamurai <mbacsamurai@hotmail.com>
2018-09-21 15:50:25 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,9 +6,9 @@ require "vendor/autoload.php";
 
 $access_token = 'YeXxLICr94+ENA0R/DHH7QmdqRJPFqTzlDW+IjLdOjiH5cqI+M6wzWKkbnCVhLMrJXnPNQuWm+ueO0DMVXV7MWjvzEPVO20Q7tl3WXll9OOomizqXwg0e5Hqt+6Dm3ucAoeKKhCksu5jlJM2P2GwFQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '8d4c20b0e8e9661750be1c0d6fe2d53e';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'Ua2b39bb7170191e0c26fb595d234bd28';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
