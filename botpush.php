5c35c0035af39f9345dd787fc51130faa958c3ef
mbacsamurai <mbacsamurai@hotmail.com>
2018-09-21 15:48:36 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,7 +4,7 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'YeXxLICr94+ENA0R/DHH7QmdqRJPFqTzlDW+IjLdOjiH5cqI+M6wzWKkbnCVhLMrJXnPNQuWm+ueO0DMVXV7MWjvzEPVO20Q7tl3WXll9OOomizqXwg0e5Hqt+6Dm3ucAoeKKhCksu5jlJM2P2GwFQdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 
