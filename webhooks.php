7455988563b7aba96df11548a2216e7edfe1547c
mbacsamurai <mbacsamurai@hotmail.com>
2018-10-11 14:24:12 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -22,9 +22,9 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type': 'sticker',
-  				'packageId': '1',
-  				'stickerId': '2560'
+				'type'=> 'sticker',
+  				'packageId'=> '1',
+  				'stickerId'=> '2560'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
