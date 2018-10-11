ca7401b56d16568ba6a3493c745760f1e65bee81
mbacsamurai <mbacsamurai@hotmail.com>
2018-10-11 13:55:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) {
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
-			$post = json_encode("SAKDA");
+			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
@@ -48,4 +48,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK";
+echo "OK2";
